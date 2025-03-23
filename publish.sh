5caf83470ae0268d96a1dd482e4e355a9de53dc1
Sebastian Schocke <sschocke@scorpio-concepts.co.za>
2025-03-23 16:17:03 +0200

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #bin/sh
 
-dotnet publish -r linux-arm /p:ShowLinkerSizeComparison=true 
+dotnet publish InverterMon.csproj -r linux-arm /p:ShowLinkerSizeComparison=true 
 rsync -r -v bin/Debug/net6.0/linux-arm/publish/ /home/pi/AxpertInverterMon/
